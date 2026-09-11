e21ed8c8d7eaf99e56ab95d37220bdcb96432ef5
Arnav Asif <ifas092@gmail.com>
2026-09-11 14:01:38 +0600

    feat: Add build configuration for manual chunking of dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor'
+            }
+            if (id.includes('gsap')) {
+              return 'gsap'
+            }
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
